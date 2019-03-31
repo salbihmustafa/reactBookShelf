67a4bd285468611299aae5935cae490783d7a3fb
salbihmustafa <salbihdmustafa@gmail.com>
2019-03-31 18:50:40 -0500

    If search bar blank then results disappear

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -10,7 +10,8 @@ class SearchBooks extends Component {
     state = {
         query: "",
         searchBooks: [],
-        books: this.props.books
+        books: this.props.books,
+        visibleText: false
     }
 
     runSearch = event => {
@@ -19,9 +20,11 @@ class SearchBooks extends Component {
         if (newText.length > 0) {
             search(newText).then(searchBooks => {
                 if (searchBooks.length > 0) {
-                    this.setState({ searchBooks });
+                    this.setState({ searchBooks, visibleText: true });
                 }
             });
+        } else {
+            this.setState({ visibleText: false });
         }
     };
 
@@ -41,34 +44,36 @@ class SearchBooks extends Component {
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid">
-                        {this.state.searchBooks.map((book, i) => (
-                            <li key={i}>
-                                <div className="book">
-                                    <div className="book-top">
-                                        <div className="book-cover"
-                                            style={{
-                                                width: 128,
-                                                height: 193,
-                                                backgroundImage: `url(${book.imageLinks.thumbnail})`
-                                            }}>
+                    {this.state.visibleText &&
+                        <ol className="books-grid">
+                            {this.state.searchBooks.map((book, i) => (
+                                <li key={i}>
+                                    <div className="book">
+                                        <div className="book-top">
+                                            <div className="book-cover"
+                                                style={{
+                                                    width: 128,
+                                                    height: 193,
+                                                    backgroundImage: `url(${book.imageLinks.thumbnail})`
+                                                }}>
+                                            </div>
+                                            <div className="book-shelf-changer">
+                                                <select value={book.shelf}>
+                                                    <option value="none" disabled>Move to...</option>
+                                                    <option value="currentlyReading">Currently Reading</option>
+                                                    <option value="wantToRead">Want to Read</option>
+                                                    <option value="read">Read</option>
+                                                    <option value="none">None</option>
+                                                </select>
+                                            </div>
                                         </div>
-                                        <div className="book-shelf-changer">
-                                            <select value={book.shelf}>
-                                                <option value="none" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                            </select>
-                                        </div>
+                                        <div className="book-title">{book.title}</div>
+                                        <div className="book-authors">{book.authors}</div>
                                     </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.authors}</div>
-                                </div>
-                            </li>
-                        ))}
-                    </ol>
+                                </li>
+                            ))}
+                        </ol>
+                    }
                 </div>
             </div>
         )

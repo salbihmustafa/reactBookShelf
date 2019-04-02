9d8ceb29f3af966ed0135172495af32eba201079
salbihmustafa <salbihdmustafa@gmail.com>
2019-04-01 22:08:54 -0500

    To each bookshelf pass onShelfChange attribute also changed to functional component.

--- src/Library.js
+++ src/Library.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
 
+//Functional Component. No rendering.
+function Library(props) {
 
-class Library extends Component {
-
-  render() {
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -13,21 +12,20 @@ class Library extends Component {
         </div>
         <div className="list-books-content">
           <Bookshelf name="Currently Reading"
-                     mappedBooks={this.props.mappedBooks}
-                     shelf={this.props.shelves.currentlyReading} />
+                     books={props.books.filter(book => book.shelf === 'currentlyReading')}
+                     onShelfChange={props.onShelfChange} />
           <Bookshelf name="Want To Read"
-                     mappedBooks={this.props.mappedBooks}
-                     shelf={this.props.shelves.wantToRead} />
+                     books={props.books.filter(book => book.shelf === 'wantToRead')} 
+                     onShelfChange={props.onShelfChange}/>
           <Bookshelf name="Read"
-                     mappedBooks={this.props.mappedBooks}
-                     shelf={this.props.shelves.read} />
+                     books={props.books.filter(book => book.shelf === 'read')} 
+                     onShelfChange={props.onShelfChange}/>
         </div>
         <div className="open-search">
           <Link to="/search">Add a book</Link>
         </div>
       </div>
     )
-  }
 }
 
 export default Library

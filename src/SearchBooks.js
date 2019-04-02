da9557b1a4b188e9431ab94ea4a244711d9f6179
salbihmustafa <salbihdmustafa@gmail.com>
2019-04-01 22:09:47 -0500

    Deleted unnecessary state. Incomplete. Need to map over book on home page and map over book on search page and match shelves

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -10,7 +10,6 @@ class SearchBooks extends Component {
     state = {
         query: "",
         searchBooks: [],
-        books: this.props.books,
         visibleText: false
     }
 
@@ -20,6 +19,10 @@ class SearchBooks extends Component {
         if (newText.length > 0) {
             search(newText).then(searchBooks => {
                 if (searchBooks.length > 0) {
+                    // map over the bookOnHomePage
+                    // map over the bookOnSearchPage
+                    // check bookOnHomePage.id === bookOnSearchPage.id
+                    // Match the shelf bookOnSearchPage.shelf = bookOnHomePage.shelf
                     this.setState({ searchBooks, visibleText: true });
                 }
             });
@@ -47,30 +50,7 @@ class SearchBooks extends Component {
                     {this.state.visibleText &&
                         <ol className="books-grid">
                             {this.state.searchBooks.map((book, i) => (
-                                <li key={i}>
-                                    <div className="book">
-                                        <div className="book-top">
-                                            <div className="book-cover"
-                                                style={{
-                                                    width: 128,
-                                                    height: 193,
-                                                    backgroundImage: `url(${book.imageLinks.thumbnail})`
-                                                }}>
-                                            </div>
-                                            <div className="book-shelf-changer">
-                                                <select value={book.shelf}>
-                                                    <option value="none" disabled>Move to...</option>
-                                                    <option value="currentlyReading">Currently Reading</option>
-                                                    <option value="wantToRead">Want to Read</option>
-                                                    <option value="read">Read</option>
-                                                    <option value="none">None</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div className="book-title">{book.title}</div>
-                                        <div className="book-authors">{book.authors}</div>
-                                    </div>
-                                </li>
+                                <Book />
                             ))}
                         </ol>
                     }
